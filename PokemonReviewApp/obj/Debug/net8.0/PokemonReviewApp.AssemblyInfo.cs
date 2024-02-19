ff2a382caca4c706d60f045712540a0b0d0755a7
PhanPhuc <phucpth2001@gmail.com>
2024-02-19 21:14:35 +0700

    delete category

--- PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1f29f81f21d40d70bac603eb1a7c40709dbef8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
